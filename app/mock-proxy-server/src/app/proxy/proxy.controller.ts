e813917f9c11a947b41ced209a57ff3a4150dd2b
김도현 <ehehwhdwhd@naver.com>
2025-12-17 14:29:39 +0900

    enable cors

--- app/mock-proxy-server/src/app/proxy/proxy.controller.ts
+++ app/mock-proxy-server/src/app/proxy/proxy.controller.ts
@@ -1,9 +1,10 @@
-import { All, Controller, HttpStatus, Query, Redirect } from '@nestjs/common';
+import { All, Controller, Header, HttpStatus, Query, Redirect } from '@nestjs/common';
 import { ApiResponse } from '@nestjs/swagger';
 
 @Controller('/proxy')
 export class ProxyController {
   @All()
+  @Header('Cache-Control', 'no-cache')
   @Redirect()
   @ApiResponse({ status: HttpStatus.FOUND })
   proxy(@Query('url') url: string) {
--- app/mock-proxy-server/src/main.ts
+++ app/mock-proxy-server/src/main.ts
@@ -6,6 +6,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableShutdownHooks();
+  app.enableCors();
 
   const options = new DocumentBuilder()
     .setTitle('mock-proxy-server swagger')
